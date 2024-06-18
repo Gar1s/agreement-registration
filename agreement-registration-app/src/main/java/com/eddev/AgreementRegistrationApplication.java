c25a0227545d797ad55013e969987cbc73127466
Eddie <partyka.eduard@gmail.com>
2024-06-18 11:39:11 +0300

    [AR-init] created a project

---
+++ agreement-registration-app/src/main/java/com/eddev/AgreementRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.eddev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgreementRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgreementRegistrationApplication.class, args);
+    }
+
+}
