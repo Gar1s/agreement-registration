955df30f95ab59b8c2852b80c919c7d776c263f4
Eddie <partyka.eduard@gmail.com>
2024-07-22 13:32:10 +0300

    [AR-32] - refactored xml

--- agreement-registration-api/src/main/java/com/eddev/dto/AgreementDto.java
+++ agreement-registration-api/src/main/java/com/eddev/dto/AgreementDto.java
@@ -18,6 +18,7 @@ public class AgreementDto {
     private LocalDate startDate;
     private LocalDate endDate;
     private String studentInitials;
+    private String numeration;
     private LocalDate companyAgreementDate;
     private String companyName;
     private String companyId;
